2eee75ea8e521c6dde2296dfef2562cc19a0f1d9
Aditya Pradhan <pradhan.aditya@gmail.com>
2012-10-02 12:22:37 -0700

    small formatting changes

--- src/TelnetCheck.rb
+++ src/TelnetCheck.rb
@@ -9,19 +9,26 @@ rescue => error
 	puts %w{there was a problem loading the net/telnet lib }
 end
 
+# change the load and use a file. Probably need to regex as well.
 HostsArray = [{'host'=> 'localhost','port'=> 18000},
 			  {'host'=> 'www.google.com', 'port'=> 80} ,
 			  {'host'=> 'localhost', 'port'=> 18001} 
 			  ]
 				
 HostsArray.each do |pair|
-	puts 'the host is : '  + pair['host'].to_s + "\n"
+	puts 'HOST: '  + pair['host'].to_s + "\n"
+	puts 'PORT: ' + pair['port'].to_s + "\n\n"
 	begin
 		Net::Telnet::new("Host" => pair['host'], "Port" => pair['port'], "Timeout" => 10 )
+		# if there is no exception then the telnet was successful.
 	rescue => e
-		puts 'Could not connect to ' + pair['host'] + "\n"
+		# an exception should be noted as a host that could not be reached due to firewall.
+		puts pair['host'].to_s + ":" + pair['port'].to_s + " -- " + e.to_s
 	end
+	puts "\n"
 end
+
+# need to collect output. Maybe as an HTML :P , that would be nice.
 
 puts "\n\n\nPress ENTER to exit"; ans = gets.chomp
 
